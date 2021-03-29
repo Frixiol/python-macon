52b65bae10ba3b8c6c108dba5051030ae16166a1
Frixiol <79292185+Frixiol@users.noreply.github.com>
2021-03-29 12:56:39 +0200

    Update macon.py

--- macon.py
+++ macon.py
@@ -60,13 +60,14 @@ def calcul():
         canwidth=500
         canheight=canwidth
 
+    squar_placex = canwidth/nbTuilelong
+    squar_placey = canheight/nbTuilelarg
 
-
-    ouver(longP, largP, squareM, nbTuile,colle,nbTuilelong,nbTuilelarg,canwidth,canheight)
+    ouver(longP,largP,longT,largT,squareM,nbTuile,colle,nbTuilelong,nbTuilelarg,canwidth,canheight,squar_placex,squar_placey)
     # insérer votre code
 
 
-def ouver(longP, largP, squareM, nbTuile,colle,nbTuilelong,nbTuilelarg,canwidth,canheight):
+def ouver(longP,largP,longT,largT,squareM,nbTuile,colle,nbTuilelong,nbTuilelarg,canwidth,canheight,squar_placex,squar_placey):
     labelShowLong1 = Label(fen, text="Longueur:", bg='#D8D5E4')
     labelShowLong1.pack()
     labelShowLong1.place(relx=0.1, rely=0.5)
@@ -105,12 +106,55 @@ def ouver(longP, largP, squareM, nbTuile,colle,nbTuilelong,nbTuilelarg,canwidth,
     fen2 = Tk()  # création d’une fenêtre Tk
     fen2.title("affiche")
     fen2.geometry("600x600")
-
-    DrawCanvas = Canvas(fen2,width=canwidth,height=canheight,bg='grey',borderwidth=0)
+    fen2.configure(bg='#D8D5E4')
+    DrawCanvas = Canvas(fen2,width=canwidth,height=canheight,bg='grey',borderwidth=0,highlightthickness=0)
     DrawCanvas.place(x=50,y=50)
 
+    for i in range(nbTuilelarg):
+        
+        if i % 2==0:
+            oddeven = 1
+        else:
+            oddeven = 2
 
-
+        for y in range(nbTuilelong):
+	
+            if oddeven == 1:				
+                if y % 2==0:
+                    pattern = 'white'
+                else:
+                    pattern = 'black'
+			
+            if oddeven == 2:	
+                if y % 2==0:
+                    pattern = 'black'
+                else:
+                    pattern = 'white'
+			
+            squar = DrawCanvas.create_rectangle(squar_placex*y,squar_placey*i,(squar_placex*y)+squar_placex,(squar_placey*i)+squar_placey,fill=pattern)
+    
+    restecutx=squar_placex-((int(longP)/longT-math.floor(int(longP)/longT))*squar_placex)
+    if restecutx == squar_placex:
+        restecutx = 0	
+		
+    restecuty=squar_placey-((int(largP)/largT-math.floor(int(largP)/largT))*squar_placey)
+    if restecuty == squar_placey:
+        restecuty = 0	
+ 
+    squarcut = Canvas(fen2,width=squar_placex,height=canheight,bg='#D8D5E4',borderwidth=0,highlightthickness=0)
+    squarcut.place(x=50+canwidth-restecutx/2,y=50)
+    squarcut = Canvas(fen2,width=squar_placex,height=canheight,bg='#D8D5E4',borderwidth=0,highlightthickness=0)
+    squarcut.place(x=50-squar_placex+restecutx/2,y=50)
+    squarcut = Canvas(fen2,width=canwidth,height=squar_placey,bg='#D8D5E4',borderwidth=0,highlightthickness=0)
+    squarcut.place(x=50,y=50+canheight-restecuty/2)
+    squarcut = Canvas(fen2,width=canwidth,height=squar_placey,bg='#D8D5E4',borderwidth=0,highlightthickness=0)
+    squarcut.place(x=50,y=50-squar_placey+restecuty/2)
+    print(squar_placex)
+    print(squar_placey)
+    print(50+canwidth-restecutx/2)
+    
+	
+	
     fen2.mainloop()
 
 
@@ -128,11 +172,15 @@ bouton1.place(relx=0.7, rely=0.15)
 
 bouton2 = Radiobutton(fen, text="30x30", variable=var, value=2, bg='#D8D5E4')
 bouton2.pack()
-bouton2.place(relx=0.7, rely=0.25)
+bouton2.place(relx=0.7, rely=0.20)
 
 bouton3 = Radiobutton(fen, text="20x40", variable=var, value=3, bg='#D8D5E4')
 bouton3.pack()
-bouton3.place(relx=0.7, rely=0.35)
+bouton3.place(relx=0.7, rely=0.25)
+
+bouton4 = Radiobutton(fen, text="Personaliser", variable=var, value=4, bg='#D8D5E4')
+bouton4.pack()
+bouton4.place(relx=0.7, rely=0.30)
 
 inputlongP = Entry(fen)
 inputlongP.pack()
@@ -142,6 +190,14 @@ inputlargP = Entry(fen)
 inputlargP.pack()
 inputlargP.place(relheight=0.06, relwidth=0.1, relx=0.2, rely=0.2)
 
+inputlonggT = Entry(fen)
+inputlonggT.pack()
+inputlonggT.place(relheight=0.06, relwidth=0.05, relx=0.7, rely=0.36)
+
+inputlarggT = Entry(fen)
+inputlarggT.pack()
+inputlarggT.place(relheight=0.06, relwidth=0.05, relx=0.8, rely=0.36)
+
 labeltxt1 = Label(fen, text="longueur:", bg='#D8D5E4')
 labeltxt1.pack()
 labeltxt1.place(relheight=0.06, relwidth=0.1, relx=0.09, rely=0.1)
@@ -149,6 +205,10 @@ labeltxt1.place(relheight=0.06, relwidth=0.1, relx=0.09, rely=0.1)
 labeltxt2 = Label(fen, text="largeur:", bg='#D8D5E4')
 labeltxt2.pack()
 labeltxt2.place(relheight=0.06, relwidth=0.1, relx=0.09, rely=0.2)
+
+labeltxt3 = Label(fen, text="X", bg='#D8D5E4')
+labeltxt3.pack()
+labeltxt3.place(relheight=0.06, relwidth=0.05, relx=0.75, rely=0.36)
 
 boutonL = Button(fen, text="Launch", command=calcul)
 boutonL.pack()
